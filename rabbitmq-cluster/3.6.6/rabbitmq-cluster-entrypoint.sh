959eb7626b06f32230d90fdc1fede8b941327b25
Alessandro Vitale <alessandro.vitale@webratio.com>
2018-02-20 16:05:08 +0100

    Some fix and optimization

--- rabbitmq-cluster/3.6.6/rabbitmq-cluster-entrypoint.sh
+++ rabbitmq-cluster/3.6.6/rabbitmq-cluster-entrypoint.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 set -x
 
-echo "Start rabbit node. Waiting network is up ..."
-sleep 5
 
+wait_network() {
 
-RABBIT_HOST=`echo $RABBITMQ_NODENAME | cut -d'@' -f 2`
+	echo "Start rabbit node. Waiting network is up ..."
+	sleep 5
+	
+	
+	RABBIT_HOST=`echo $RABBITMQ_NODENAME | cut -d'@' -f 2`
+	
+	if [ -z "$RABBIT_HOST" ]; then
+	    echo "Wrong $RABBITMQ_NODENAME"
+	    exit 1
+	fi
+	
+	n=1
+	while [[ $(drill $RABBIT_HOST | grep $RABBIT_HOST | tail -n +2 | awk '{print $5}' | grep '^10.0' | wc -c) -eq 0 ]] && [[ $n -le 10 ]]
+	do
+	     n=$(( n+1 ))
+	     sleep 5
+	done
+	
+	
+	if [ $n -gt 10 ]; then
+	    echo "Problem to resolv IP for $RABBITMQ_NODENAME"
+	    exit 1
+	fi
+	
+	IP_RESOLV=$(drill $RABBIT_HOST | grep $RABBIT_HOST | tail -n +2 | awk '{print $5}')
+	
+	
+	echo "Rabbit IP $IP_RESOLV for Rabbit nodename $RABBITMQ_NODENAME" 
 
-if [ -z "$RABBIT_HOST" ]; then
-    echo "Wrong $RABBITMQ_NODENAME"
-    exit 1
-fi
-
-n=1
-while [[ $(drill $RABBIT_HOST | grep $RABBIT_HOST | tail -n +2 | awk '{print $5}' | grep '^10.0' | wc -c) -eq 0 ]] && [[ $n -le 10 ]]
-do
-     n=$(( n+1 ))
-     sleep 5
-done
-
-
-if [ $n -gt 10 ]; then
-    echo "Problem to resolv IP for $RABBITMQ_NODENAME"
-    exit 1
-fi
-
-IP_RESOLV=$(drill $RABBIT_HOST | grep $RABBIT_HOST | tail -n +2 | awk '{print $5}')
-
-
-echo "Rabbit IP $IP_RESOLV for Rabbit nodename $RABBITMQ_NODENAME" 
-
+}
 
 setup_cluster() {
 	
@@ -47,6 +51,9 @@ setup_cluster() {
 		rabbitmqctl start_app &
 	fi
 }
+
+
+wait_network
 
 setup_cluster
 
